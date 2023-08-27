3f44c21c75fabbacfc7de83dda1259b6c8426c7a
yyeennyy <kye3314@gmail.com>
2023-08-27 09:30:17 +0900

    EntityManager를 생성자주입으로 변경

--- src/main/java/jpastudy/jpashop/repository/MemberRepository.java
+++ src/main/java/jpastudy/jpashop/repository/MemberRepository.java
@@ -1,16 +1,22 @@
 package jpastudy.jpashop.repository;
 
 import jpastudy.jpashop.domain.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+//    @PersistenceContext
+//    @Autowired // @PersistenceContext말고 @Autowired해도 됨. 그치만 근본적으로 EM은 @Per.. 가 있어야 injection이 되는데, SpringBoot는 @Autowired도 가능함. by Spring Data JPA
+//    private EntityManager em;
+
+    private final EntityManager em; // 생성자 injection
 
     public Member findOne(Long id){
         return em.find(Member.class, id);
@@ -26,5 +32,7 @@ public class MemberRepository {
                 .setParameter("name", name)
                 .getResultList();
     }
-    
+
+    public void save(Member member) {
+    }
 }
